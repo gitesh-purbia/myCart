2ce3f45bfa82c2a7c06f4633039bb3318c3a6b0d
Gitesh Purbia <gitesh.purbiya22@gmail.com>
2018-02-23 13:45:07 +0530

    update sever file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const path = require('path');
+
 const app = express();
 
-app.configure(() => {
-  app.use(express.bodyParser());
-  app.use(express.static(path.join(__dirname, 'dist')));
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 5000;
+
 app.listen(port);
+
 console.log('server started ' + port);
