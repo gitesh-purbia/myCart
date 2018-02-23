40718d63fb517ce8010ed7997bf25955fba3c75a
Gitesh Purbia <gitesh.purbiya22@gmail.com>
2018-02-23 20:51:43 +0530

    jquery added

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   extends: [
     'angular'
   ],
+  globals: {
+    window: true
+  },
   rules: {
-    'angular/no-service-method': 0
+    'angular/no-service-method': 0,
+    'angular/window-service': 0
   }
 }
